58f6f6bfb8849929396d5300a71d966e1373b653
Mandeep KHAIRA <50768465+mandeepsinghkhaira@users.noreply.github.com>
2020-11-27 02:13:17 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.listen(PORT, function () {
 });
 
 //******************posts data into the database********************
+//////////////////CUSTOMER
 app.post("/register", function (req, res) {
     let customer = {
         id: null,
@@ -31,24 +32,45 @@ app.post("/register", function (req, res) {
         PHONE_NUMBER: req.body.PHONE_NUMBER,
         EMAIL: req.body.EMAIL
     }
-    con.query('INSERT INTO CUSTOMER SET ?', customer, function (err, result) {
+    con.query('INSERT INTO CUSTOMER SET ?', customer, function (err, data) {
         if (err) throw err;
-        res.render("customer")
-        console.log(result);
+        res.redirect("/customerReport")
+        console.log(data);
+    });
+});
+////////////////////STAFF
+app.post("/registerStaff", function (req, res) {
+    let staff = {
+        id: null,
+        FNAME: req.body.FNAME,
+        SNAME: req.body.SNAME,
+        PHONE_NUMBER: req.body.PHONE_NUMBER,
+        EMAIL: req.body.EMAIL
+    }
+    con.query('INSERT INTO STAFF SET ?', staff, function (err, data) {
+        if (err) throw err;
+        res.redirect("/staffReport")
+        console.log(data);
+    });
+});
+///////////////////appointment
+app.post("/registerAppointment", function (req, res) {
+    let customer = {
+        id: null,
+        FNAME: req.body.OPTION,
+        SNAME: req.body.SNAME,
+        PHONE_NUMBER: req.body.PHONE_NUMBER,
+        EMAIL: req.body.EMAIL
+    }
+    con.query('INSERT INTO CUSTOMER SET ?', customer, function (err, data) {
+        if (err) throw err;
+        res.redirect("/appointment")
+        console.log(data);
     });
 });
 
 //*****************routes********************
-
-app.get('/', function (req, res) {
-    let q = "SELECT * FROM CUSTOMER";
-    con.query(q, function (err, data) {
-        if (err) throw err;
-
-        res.render("customer", { title: 'view', data: data });
-    })
-});
-
+/////////////////CUSTOMER
 app.get('/customerReport', function (req, res) {
     let q = "SELECT * FROM CUSTOMER";
     con.query(q, function (err, data) {
@@ -57,7 +79,27 @@ app.get('/customerReport', function (req, res) {
         res.render("customerReport", { title: 'customerReport', data: data });
     })
 });
-//*******************delete data
+//////////////////////STAFF
+app.get('/staffReport', function (req, res) {
+    let q = "SELECT * FROM STAFF";
+    con.query(q, function (err, data) {
+        if (err) throw err;
+
+        res.render("staffReport", { title: 'staffReport', data: data });
+    })
+});
+///////////////////////////appointment
+app.get('/appointment', function (req, res) {
+    let q = "SELECT * FROM CUSTOMER";
+    con.query(q, function (err, data) {
+        if (err) throw err;
+
+        res.render("appointment", { title: 'customerReport', data: data });
+    })
+});
+//*******************delete data************************************
+
+///////////////////////CUSTOMER
 app.get('/delete/(:id)', function (req, res, next) {
     let id = req.params.id;
     let z = `DELETE FROM CUSTOMER WHERE id=${id}`;
@@ -66,5 +108,16 @@ app.get('/delete/(:id)', function (req, res, next) {
         console.log(data.affectedRows + " record(s) updated");
     });
     res.redirect('/customerReport');
+
+});
+/////////////////////////STAFF
+app.get('/erase/(:id)', function (req, res, next) {
+    let id = req.params.id;
+    let z = `DELETE FROM STAFF WHERE id=${id}`;
+    con.query(z, function (err, data) {
+        if (err) throw err;
+        console.log(data.affectedRows + " record(s) updated");
+    });
+    res.redirect('/staffReport');
 
 });
